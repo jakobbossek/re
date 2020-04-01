0aa0ea6545e15e6c89723be328949353671f0478
Jakob Bossek <info@jakobbossek.de>
2020-04-01 22:17:35 +1030

    add rowToList

--- R/rowToList.R
+++ R/rowToList.R
@@ -1,4 +1,36 @@
-rowToList = function(x, named = TRUE) {
+#' @title Convert data frame rows to list.
+#'
+#' @description Returns a list of lists with each sub-list being a
+#' row of the data frame.
+#'
+#' @param x [\code{data.frame}]\cr
+#'   Data frame.
+#' @param named [\code{logical(1)}]\cr
+#'   Should the sub-lists be named?
+#'   If \code{TRUE} the \code{colnames(x)} are used if available.
+#'   Otherwise names \dQuote{V1, ...,VX} are used where X equals
+#'   \code{ncol(x)}.
+#'   Default is \code{TRUE}.
+#' @return [\code{list}] List of (named) lists.
+#' @examples
+#' x = data.frame(x = 1:3, y = letters[3:5])
+#' rowsToList(x)
+#' rowsToList(x, named = FALSE)
+#' rowsToList(unname(x), named = FALSE)
+#' @export
+rowsToList = function(x, named = TRUE) {
   checkmate::assertDataFrame(x, min.rows = 1L)
-  lapply(seq_len(nrow(x)), function(i) as.list(x[i, ]))
+  ns = colnames(x)
+  is.named = !is.null(ns)
+  nc = ncol(x)
+  lapply(seq_len(nrow(x)), function(i) {
+    l = unname(as.list(x[i, ]))
+    if (named) {
+      if (is.named)
+        names(l) = ns
+      else
+        names(l) = paste0("X", 1:nc)
+    }
+    return(l)
+  })
 }
