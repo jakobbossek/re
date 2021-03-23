bc99c888f33c4095e477613834c21434b942ff8a
Jakob Bossek <info@jakobbossek.de>
2021-03-23 02:43:41 +0100

    fs_drop_ext now works for vectors; fs_drop_ext now can be passed an optional character vector of file extensions

--- R/fs_drop_ext.R
+++ R/fs_drop_ext.R
@@ -7,26 +7,47 @@
 #'   split the actual name from the file extension. Defaults to \dQuote{first},
 #'   i.e., the first dot is used. Option \dQuote{last} anologously uses the last
 #'   dot. Alternatively, one can pass a single integer specifying the position.
+#' @param ext [\code{character(1)}]\cr
+#'   Optional explicit character vector of file extension(s) to drop without
+#'   leading dot.
+#'   Default is \code{NULL}.
 #' @return [\code{character}]
 #' @export
-fs_drop_ext = function(path, pos = "first") {
+fs_drop_ext = function(path, pos = "first", ext = NULL) {
+  checkmate::assert_character(ext, min.len = 1L, min.chars = 1L, any.missing = FALSE, all.missing = FALSE, null.ok = TRUE)
   if (!is.numeric(pos))
     checkmate::assert_choice(pos, choices = c("first", "last"))
   else
     pos = checkmate::asInt(pos, lower = 1L)
   dn = fs_dirname(path)
   bn = basename(path)
-  dot_pos = unlist(gregexpr(pattern = '\\.', bn))
-  n_dots = length(dot_pos)
-  if (is.numeric(pos) && n_dots < pos)
-    re::stopf("[fs_drop_ext] There are only %i dots in '%s', but pos(ition) %i passed.", n_dots, path, pos)
-  if (n_dots == 0L)
-    return(path)
-  if (pos == "first")
-    dot_pos = dot_pos[1L]
-  else if (pos == "last")
-    dot_pos = dot_pos[n_dots]
-  else
-    dot_pos = dot_pos[pos]
+
+  # if explicit file extension is passed, drop these and return
+  if (!is.null(ext)) {
+    pattern = paste0("(", collapse(paste0("\\.", ext), sep = "|"), ")")
+    return(file.path(dn, gsub(pattern, "", bn)))
+  }
+
+  dot_pos = gregexpr(pattern = '\\.', bn)
+  dot_pos = sapply(dot_pos, function(dp) {
+    n_dots = length(dp)
+    if (is.numeric(pos) && n_dots < pos)
+      re::stopf("[fs_drop_ext] There are only %i dots in '%s', but pos(ition) %i passed.", n_dots, path, pos)
+    if (n_dots == 0L || dp[1L] == -1)
+      dp = NA_integer_
+    if (pos == "first")
+      dp = dp[1L]
+    else if (pos == "last")
+      dp = dp[n_dots]
+    else
+      dp = dp[pos]
+    return(dp)
+  })
+
+  idx.na = which(is.na(dot_pos))
+  if (length(idx.na) > 0L) {
+    dot_pos[idx.na] = sapply(bn[idx.na], nchar) + 1L
+  }
+
   file.path(dn, substr(bn, 1L, dot_pos - 1L))
 }
--- tests/testthat/test_fs_drop_ext.R
+++ tests/testthat/test_fs_drop_ext.R
@@ -7,4 +7,13 @@ test_that("fs_drop_ext works as expected", {
   testthat::expect_equal(fs_drop_ext(x, pos = 3L), "this/is/a/file.with.many")
   testthat::expect_equal(fs_drop_ext(x, pos = 2L), "this/is/a/file.with")
   testthat::expect_equal(fs_drop_ext(x, pos = 1), "this/is/a/file")
+  # expicit extensions
+  testthat::expect_equal(fs_drop_ext(x, ext = "extensions"), "this/is/a/file.with.many")
+  testthat::expect_equal(fs_drop_ext(x, ext = "many.extensions"), "this/is/a/file.with")
+  testthat::expect_equal(fs_drop_ext(x, ext = "many.extensions"), "this/is/a/file.with")
+  testthat::expect_error(fs_drop_ext(x, ext = ""))
+
+  x = list.files(system.file(".", package = "re"))
+  n = length(x)
+  checkmate::expect_character(fs_drop_ext(x), len = n)
 })
